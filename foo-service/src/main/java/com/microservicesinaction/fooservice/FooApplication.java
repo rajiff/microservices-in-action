8adc38edda90aecc09a4e91c6edc40344adbef86
Basavaraj K N <Basavaraj.K@Stackroute.in>
2019-09-22 20:30:32 +0530

    added discover service, a sample rest resource service

---
+++ foo-service/src/main/java/com/microservicesinaction/fooservice/FooApplication.java
@@ -0,0 +1,14 @@
+package com.microservicesinaction.fooservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooApplication.class, args);
+    }
+}
