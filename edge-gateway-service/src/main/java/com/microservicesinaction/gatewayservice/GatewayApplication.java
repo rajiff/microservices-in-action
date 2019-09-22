7ed1ede6b4b666ab63a012cda6401dee7aa052f7
Basavaraj K N <Basavaraj.K@Stackroute.in>
2019-09-22 17:24:32 +0530

    initial commit - zuul gateway

---
+++ edge-gateway-service/src/main/java/com/microservicesinaction/gatewayservice/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.microservicesinaction.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.microservicesinaction.gatewayservice.fitlers.pre.GatewayLoggingFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+  
+  @Bean
+  public GatewayLoggingFilter loggingFilter() {
+    return new GatewayLoggingFilter();
+  }
+
+}
